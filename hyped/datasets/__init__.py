db186c7adf7136b58434f4fa6a49d16a7ab586da
Niclas Doll <niclas.doll@iais.fraunhofer.de>
2023-05-16 02:27:39 +0200

    Implemented AmazonCat datasets using extreme classification repository

--- hyped/datasets/__init__.py
+++ hyped/datasets/__init__.py
@@ -1,6 +1,7 @@
 import inspect
 import datasets
 from . import cas
+from . import xcr
 
 # for easy of use
 load_dataset = datasets.load_dataset
@@ -9,5 +10,6 @@ _hash_python_lines = datasets.packaged_modules._hash_python_lines
 _PACKAGED_DATASETS_MODULES = datasets.packaged_modules._PACKAGED_DATASETS_MODULES
 # register datasets
 _PACKAGED_DATASETS_MODULES.update({
-    'hyped.datasets.cas': (cas.__name__, _hash_python_lines(inspect.getsource(cas).splitlines()))
+    'hyped.datasets.cas': (cas.__name__, _hash_python_lines(inspect.getsource(cas).splitlines())),
+    'hyped.datasets.xcr': (xcr.__name__, _hash_python_lines(inspect.getsource(xcr).splitlines()))
 })
